d011f1d4f64ecda6cba3fac0923a70e14bac55f0
Fabiano Fernandes <lfabianocf@gmail.com>
2024-06-18 15:45:20 -0300

    paciente-controller-atualiza-criado-dto-retornar-dados-paciente-atualizados

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,17 +33,23 @@ public class PacienteController {
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
 
         var paciente = repository.getReferenceById(dados.id());
+
         paciente.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
 
         var paciente = repository.getReferenceById(id);
         paciente.excluir();
+
+        // Retorna código 204 Requisição processada e sem retorno
+        return ResponseEntity.noContent().build();
     }
-}
+;}
