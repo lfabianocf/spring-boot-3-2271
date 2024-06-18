bfef5feed889ebc8e273215009bc438e942988ca
Fabiano Fernandes <lfabianocf@gmail.com>
2024-06-18 13:43:10 -0300

    medico-excluir-retorno-Spring-ResposenEntity-204

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -34,7 +35,6 @@ public class MedicoController {
     @GetMapping
     public Page<DadosListagemMedico> lista(@PageableDefault(size=10, sort = {"nome"}) Pageable paginacao) {
         //return  repository.findAll(paginacao).stream().map(DadosListagemMedico::new).toList();
-
         // return  repository.findAll(paginacao).map(DadosListagemMedico::new);
 
         return  repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
@@ -53,14 +53,13 @@ public class MedicoController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
 
         var medico = repository.getReferenceById(id);
-
-        medico.excluir();
-
         // exclusão fisica.
         //repository.deleteById(id);
+        medico.excluir();
 
+        return  ResponseEntity.noContent().build();
     }
 }
