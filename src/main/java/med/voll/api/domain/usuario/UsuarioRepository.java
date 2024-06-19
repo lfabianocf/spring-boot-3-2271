e99b38275079e58032bcee8b16b23f5f2a752ffa
Fabiano Fernandes <lfabianocf@gmail.com>
2024-06-19 11:36:35 -0300

    class-usuario-repository-criada

---
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
+}
