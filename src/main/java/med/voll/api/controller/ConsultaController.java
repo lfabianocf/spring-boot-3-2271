e01728df2956da94aeea1dfa2f3d10bc6ddf8aaf
Fabiano Fernandes <lfabianocf@gmail.com>
2024-07-04 14:53:57 -0300

    classe-agenaDeConsulta-criada-para-validar-regras-negocios

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -3,8 +3,10 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsulta;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsulta agenda;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
 
-        System.out.println(dados);
+        //System.out.println(dados);
+
+        agenda.agendar(dados);
 
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null,null,null));
 
